0a8ba13fb66fdcf151259e10e558da52a62e8230
Juanandres44 <60269704+Juanandres44@users.noreply.github.com>
2020-10-20 18:27:11 -0500

    Taller 1 TypeScript

---
+++ taller1/scripts/dataCourses.js
@@ -0,0 +1,10 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de SW en equipo", "Rubby Casallas", 3),
+    new Course("Lenguajes y Maquinas", "Silvia Takahashi", 3),
+    new Course("Diseño de Algoritmos", "Rodrigo Cardozo", 3),
+    new Course("Calculo Vectorial", "Paul Bressler", 3),
+    new Course("Fisica", "Carlos Andrés Florez", 3),
+    new Course("Francés", "Jenny Elizabeth Muñoz", 3),
+    new Course("Deporte", "Cristian Aristizabal", 1)
+];
